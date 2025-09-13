b8a69015a215e5429ca219649281b5b6851239c0
Shibasankar Rout <shibasankar@Shibasankars-MacBook-Air.local>
2025-09-14 01:06:35 +0530

    first commit

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -220,13 +220,10 @@ resource "aws_iam_policy" "presign_lambda_policy" {
       {
         Action = [
           "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket"
-
+          "s3:GetObject"
         ]
         Effect   = "Allow"
-        Resource = "arn:aws:s3:::${var.upload_bucket_name}/*",
-        Resource = "arn:aws:s3:::shibasankar-accelerator-upload-bucket/*"
+        Resource = "arn:aws:s3:::${var.upload_bucket_name}/*"
       }
     ]
   })
